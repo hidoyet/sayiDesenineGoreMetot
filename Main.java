c0b9b41a55886f19cc33b0aa90f6278d88f92446
hidoyet <102352895+hidoyet@users.noreply.github.com>
2022-04-11 14:42:40 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,43 @@
+package sayiDesenineGoreMetot;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.print("Sayýyý giriniz: ");
+		int num=scanner.nextInt();
+		System.out.print("Kaçlý desen olacak: ");
+		int patNum=scanner.nextInt();
+		int temp=num;
+		patternMinus(num, patNum,temp);
+	}
+	static int patternMinus(int num,int patNum,int temp) {
+		
+		System.out.print(num+" ");
+		if(num<=0) {
+			return patternPlus(num+patNum, patNum,temp);
+		}else
+		return patternMinus(num-patNum, patNum, temp);
+		
+		
+	}
+	static int patternPlus(int num,int patNum,int temp) {
+		System.out.print(num+" ");
+		if(num==temp) {
+			return 0;
+		}else 
+		return patternPlus(num+patNum, patNum, temp);
+		
+		
+		
+		
+		
+		
+		
+	}
+		
+	
+
+}
